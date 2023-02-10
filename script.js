265109a99e9f824c8ba36d4a4197ff081e07cef9
Dastan <dastangosu@gmail.com>
2023-02-11 01:21:05 +0600

    Add playRound() and Game()

--- script.js
+++ script.js
@@ -3,16 +3,103 @@ function getRndInteger(min, max) {
 }
 
 function getComputerChoice() {
-  randomNumber = getRndInteger(1, 4);
+  let randomNumber = getRndInteger(1, 4);
   if (randomNumber === 1) {
-    return "Rock";
+    return "rock";
   }
   else if (randomNumber === 2) {
-    return "Paper";
+    return "paper";
   }
   else {
-    return "Scissors";
+    return "scissors";
   }
 }
 
-console.log(getComputerChoice());
+function playRound(computerSelection, playerSelection) {
+
+  if (playerSelection === "rock") {
+    switch(computerSelection) {
+      case "rock":
+        return "draw";
+        break;
+      case "paper":
+        return "comp";
+        break;
+      case "scissors":
+        return "player"
+        break;  
+    }
+  }
+  else if (playerSelection === "paper") {
+    switch(computerSelection) {
+      case "rock":
+        return "player";
+        break;
+      case "paper":
+        return "draw";
+        break;
+      case "scissors":
+        return "comp"
+        break;  
+    }
+  }
+  else {
+    switch(computerSelection) {
+      case "rock":
+        return "comp";
+        break;
+      case "paper":
+        return "player";
+        break;
+      case "scissors":
+        return "draw"
+        break;  
+    }
+  }
+}
+
+function game() {
+  let playerScore = 0;
+  let compScore = 0;
+  for(let i = 0; i<5; i++) {
+    if(playerScore === 3) {
+      console.log("Player Wins the Game!") ;
+      return;
+    }
+    else if (compScore ===3) {
+      console.log("Computer Wins the Game!");
+      return;
+    }
+    else {
+      let playerSelection = prompt("Choose a hand: rock, paper, or scissors?").toLowerCase()
+      while(playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
+        playerSelection = prompt("Incorrect input. Choose a hand: rock, paper, or scissors?").toLowerCase()
+      }
+      let computerSelection = getComputerChoice();
+      let roundResult = playRound(computerSelection,playerSelection);
+      if(roundResult === "player") {
+        console.log("Player wins the round!")
+        playerScore++;
+      }
+      else if (roundResult === "comp") {
+        console.log("Computer wins the round!")
+        compScore++;
+      }
+      else {
+        console.log("Draw round!")
+      }
+    }
+  }
+  if(compScore>playerScore) {
+    console.log("Computer Wins the Game!")
+  }
+  else if (compScore<playerScore) {
+    console.log("Player Wins the Game!")
+  }
+  else {
+    console.log("Draw Game!")
+  }
+}
+game();
+
+
